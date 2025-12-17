94b538f86ba81d26a9a140bb20d830db8c1e5426
oxanashva <oxanashav@gmail.com>
2025-12-18 01:59:51 +0200

    fix(board-service): correct getEmptyBoard and getDefaultFilter functions

--- src/services/board/index.js
+++ src/services/board/index.js
@@ -1,23 +1,31 @@
 const { DEV, VITE_LOCAL } = import.meta.env
 
-import { getRandomIntInclusive, makeId } from '../util.service'
-
 import { boardService as local } from './board.service.local'
 import { boardService as remote } from './board.service.remote'
 
-function getEmptyCar() {
+function getEmptyBoard() {
     return {
-        _id: '',
-        vendor: makeId(),
-        speed: getRandomIntInclusive(80, 240),
-        msgs: [],
+        name: '',
+        desc: '',
+        closed: false,
+        dateClosed: null,
+        starred: false,
+        prefs: {
+            background: '#1868DB',
+        },
+        idMemberCreator: '',
+        actions: [],
+        groups: [],
+        tasks: [],
+        labels: [],
+        members: [],
+        uploadedImages: []
     }
 }
 
 function getDefaultFilter() {
     return {
         txt: '',
-        minSpeed: '',
         sortField: '',
         sortDir: '',
     }
@@ -26,7 +34,7 @@ function getDefaultFilter() {
 const service = (VITE_LOCAL === 'true') ? local : remote
 
 
-export const boardService = { getEmptyCar, getDefaultFilter, ...service }
+export const boardService = { getEmptyBoard, getDefaultFilter, ...service }
 
 // Easy access to this service from the dev tools console
 // when using script - dev / dev:local
